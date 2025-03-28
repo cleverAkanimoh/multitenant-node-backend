fcf610aa34624091f5a222426849bb6088b2bf14
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-03-28 18:35:06 +0100

    modified index.ts in routes

--- src/apps/company/routes/index.ts
+++ src/apps/company/routes/index.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.get("/current", authenticate as any, CompanyController.current);
 
 router.get("/", CompanyController.list);
-router.post("/", CompanyController.create);
+// router.post("/", CompanyController.create);
 router.get("/:id", CompanyController.retrieve);
 router.put("/:id", CompanyController.update);
 router.delete("/:id", CompanyController.destroy);

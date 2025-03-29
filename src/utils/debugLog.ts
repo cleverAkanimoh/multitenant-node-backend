68b49bc927e9d97f71562d6384fde15e4a74c8e6
Muhyideeeen <108088294+Muhyideeeen@users.noreply.github.com>
2025-03-29 13:23:56 +0100

    Update debugLog.ts

--- src/utils/debugLog.ts
+++ src/utils/debugLog.ts
@@ -1,5 +1,6 @@
 export const debugLog = (message?: any, ...optionalParams: any[]) => {
-  if (process.env.NODE_ENV === "development") {
+
+//  if (process.env.NODE_ENV === "development") {
     console.log(message, ...optionalParams);
-  }
+//  }
 };

7cda2da21bf4ff770fb9bfac2167c47bd89fe582
cleverakanimoh <cleverakanimoh02@gmail.com>
2025-04-01 20:55:40 +0100

    modified 3 files

--- src/apps/(dashboard)/employees/routes/peopleRoutes.ts
+++ src/apps/(dashboard)/employees/routes/peopleRoutes.ts
@@ -1,20 +1,13 @@
 import { Router } from "express";
-import {
-    createPeople,
-    deletePeople,
-    getAllPeople,
-    getPeopleById,
-    peopleUpload,
-    updatePeople,
-} from "../controllers";
+import PeopleController from "../controllers";
 
 const router = Router();
 
-router.get("/dashboard/employees", getAllPeople);
-router.post("/dashboard/employees", createPeople);
-router.get("/dashboard/employees/:id", getPeopleById);
-router.put("/dashboard/employees/:id", updatePeople);
-router.delete("/dashboard/employees/:id", deletePeople);
-router.post("/dashboard/people/bulk-upload", peopleUpload);
+router.get("/dashboard/employees", PeopleController.list);
+router.post("/dashboard/employees", PeopleController.create);
+router.get("/dashboard/employees/:id", PeopleController.retrieve);
+router.put("/dashboard/employees/:id", PeopleController.update);
+router.delete("/dashboard/employees/:id", PeopleController.destroy);
+router.post("/dashboard/people/bulk-upload", PeopleController.bulkUpload);
 
 export default router;
--- src/apps/(dashboard)/employees/schemas/index.ts
+++ src/apps/(dashboard)/employees/schemas/index.ts
@@ -2,49 +2,49 @@ const Joi = require("joi");
 
 // Define the schema
 const peopleSchema = Joi.object({
-  firstname: Joi.string().min(1).max(255).required(),
-  lastname: Joi.string().min(1).max(255).required(),
-  phone_number: Joi.string()
+  firstName: Joi.string().min(1).max(255).required(),
+  lastName: Joi.string().min(1).max(255).required(),
+  phoneNumber: Joi.string()
     .pattern(/^\d{10}$/)
-    .required(), // 10-digit phone number
-  official_email: Joi.string().email().required(),
-  personal_phone: Joi.string().pattern(/^\d{10}$/), // Optional personal phone
-  personal_email: Joi.string().email(),
+    .required(),
+  officialEmail: Joi.string().email().required(),
+  personalPhone: Joi.string().pattern(/^\d{10}$/),
+  personalEmail: Joi.string().email(),
   address: Joi.string().min(1).max(255).required(),
-  date_of_birth: Joi.date().less("now").required(), // Ensure the date is in the past
+  dateOfBirth: Joi.date().less("now").required(),
   education: Joi.object({
     institutions: Joi.array().items(Joi.string().min(1).max(255)).required(),
     years: Joi.array().items(Joi.string().min(1).max(255)).required(),
     qualifications: Joi.array().items(Joi.string().min(1).max(255)).required(),
   }).required(),
   guarantor1: Joi.object({
-    firstname: Joi.string().min(1).max(255).required(),
-    lastname: Joi.string().min(1).max(255).required(),
+    firstName: Joi.string().min(1).max(255).required(),
+    lastName: Joi.string().min(1).max(255).required(),
     address: Joi.string().min(1).max(255).required(),
     occupation: Joi.string().min(1).max(255).required(),
-    age: Joi.number().min(18).required(), // Ensure age is at least 18
+    age: Joi.number().min(18).required(),
   }).required(),
   guarantor2: Joi.object({
-    firstname: Joi.string().min(1).max(255).required(),
-    lastname: Joi.string().min(1).max(255).required(),
+    firstName: Joi.string().min(1).max(255).required(),
+    lastName: Joi.string().min(1).max(255).required(),
     address: Joi.string().min(1).max(255).required(),
     occupation: Joi.string().min(1).max(255).required(),
-    age: Joi.number().min(18).required(), // Ensure age is at least 18
+    age: Joi.number().min(18).required(),
   }).required(),
   description: Joi.string().max(500),
   role: Joi.string().min(1).max(255).required(),
-  date_employed: Joi.date().less("now").required(), // Ensure the date is in the past
-  corporate_name: Joi.string().min(1).max(255).required(),
-  division_name: Joi.string().min(1).max(255).required(),
-  group_name: Joi.string().min(1).max(255).required(),
-  department_name: Joi.string().min(1).max(255).required(),
-  unit_name: Joi.string().min(1).max(255).required(),
-  career_level: Joi.string().min(1).max(255).required(),
-  designation_name: Joi.string().min(1).max(255).required(),
+  dateEmployed: Joi.date().less("now").required(),
+  corporateName: Joi.string().min(1).max(255).required(),
+  divisionName: Joi.string().min(1).max(255).required(),
+  groupName: Joi.string().min(1).max(255).required(),
+  departmentName: Joi.string().min(1).max(255).required(),
+  unitName: Joi.string().min(1).max(255).required(),
+  careerLevel: Joi.string().min(1).max(255).required(),
+  designationName: Joi.string().min(1).max(255).required(),
 });
 
 // Example validation function
-const validatePeople = (peopleData) => {
+const validatePeople = (peopleData: any) => {
   const { error, value } = peopleSchema.validate(peopleData);
   if (error) {
     console.log("Validation Error:", error.details);
